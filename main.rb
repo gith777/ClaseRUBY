c5460a83270ac1c27c99942ec19fb5ed1779fa7e
Manuel Venegas <manuelalexander08@gmail.com>
2018-09-28 15:36:20 -0500

    Inicializando - clase RUBY

---
+++ main.rb
@@ -0,0 +1,35 @@
+#nombre_usuario = "Yefersson"
+#puts nombre_usuario
+
+#edad=27
+#puts edad+5
+
+#puts 10.to_f/3.to_f
+
+#puts "Hola #{nombre_usuario}, \"bienvenido\""
+
+#puts nombre_usuario.methods
+
+#---------- 
+
+#Hay que colocar el to_f para definir el tipo de dato 
+#Si, NO! se coloca el tiopo de dato es solamente lo toma
+#como una cadena de texto y lo concatena tal cual...
+puts "Ingresa un numero"
+numero_uno = gets.chomp.to_f
+
+puts" Ingresa otro número"
+numero_dos = gets.chomp.to_f
+
+#puts "La suma de estos números es #{numero_uno+numero_dos}"
+#puts "El numero ingresado fue #{numero_uno}. Gracias!"
+
+if numero_uno>numero_dos
+	puts "El número uno es MAYOR! que el número dos"
+elsif numero_uno==numero_dos
+	puts "Los números son iguales"
+elsif numero_dos>numero_uno
+	puts "El número dos es MAYOR! que el número uno"	
+else
+	puts "El número uno no es mayor que el segundo número"
+end
