9601c0cf1267cb1861b38625fdcb6ec45a74a62d
Manuel Venegas <manuelalexander08@gmail.com>
2018-09-28 15:42:42 -0500

    Agregamos el case

--- main.rb
+++ main.rb
@@ -15,21 +15,43 @@
 #Hay que colocar el to_f para definir el tipo de dato 
 #Si, NO! se coloca el tiopo de dato es solamente lo toma
 #como una cadena de texto y lo concatena tal cual...
-puts "Ingresa un numero"
-numero_uno = gets.chomp.to_f
+#puts "Ingresa un numero"
+#numero_uno = gets.chomp.to_f
 
-puts" Ingresa otro número"
-numero_dos = gets.chomp.to_f
+#puts" Ingresa otro número"
+#numero_dos = gets.chomp.to_f
 
 #puts "La suma de estos números es #{numero_uno+numero_dos}"
 #puts "El numero ingresado fue #{numero_uno}. Gracias!"
 
-if numero_uno>numero_dos
-	puts "El número uno es MAYOR! que el número dos"
-elsif numero_uno==numero_dos
-	puts "Los números son iguales"
-elsif numero_dos>numero_uno
-	puts "El número dos es MAYOR! que el número uno"	
-else
-	puts "El número uno no es mayor que el segundo número"
+#if numero_uno>numero_dos
+	#puts "El número uno es MAYOR! que el número dos"
+#elsif numero_uno==numero_dos
+	#puts "Los números son iguales"
+#elsif numero_dos>numero_uno
+	#puts "El número dos es MAYOR! que el número uno"	
+#else
+	#puts "El número uno no es mayor que el segundo número"
+#end
+
+puts "Ingresa tu calificación:"
+
+calificacion = gets.chomp.to_i
+
+case calificacion
+	when 5
+		puts "Excelente"
+	when 4
+		puts "Sobresaliente"
+	when 3
+		puts "Aceptable"
+	when 2
+		puts "Insuficiente"
+	when 1, 0
+		puts "Deficiente"
+	else
+		puts "NOTA! NO valida"
 end
+
+
+
